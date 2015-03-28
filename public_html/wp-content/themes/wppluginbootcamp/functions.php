fbb6309ae98751289908cc187d36beb1f2150404
Yaron Guez <yaron.guez@gmail.com>
2015-03-28 12:06:54 -0700

    Installed what the file and setup post type

---
+++ public_html/wp-content/themes/wppluginbootcamp/functions.php
@@ -0,0 +1,45 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'wppb_theme_enqueue_styles' );
+function wppb_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
+}
+
+function wppb_setup_post_types(){
+    $labels = array(
+        'name' => _x( 'Books', 'books' ),
+        'singular_name' => _x( 'Book', 'books' ),
+        'add_new' => _x( 'Add New', 'books' ),
+        'add_new_item' => _x( 'Add New Book', 'books' ),
+        'edit_item' => _x( 'Edit Book', 'books' ),
+        'new_item' => _x( 'New Book', 'books' ),
+        'view_item' => _x( 'View Book', 'books' ),
+        'search_items' => _x( 'Search Books', 'books' ),
+        'not_found' => _x( 'No books found', 'books' ),
+        'not_found_in_trash' => _x( 'No books found in Trash', 'books' ),
+        'parent_item_colon' => _x( 'Parent Book:', 'books' ),
+        'menu_name' => _x( 'Books', 'books' ),
+    );
+    $args = array(
+        'labels' => $labels,
+        'hierarchical' => false,
+        'supports' => array( 'title', 'editor', 'excerpt', 'thumbnail', 'custom-fields', 'comments', 'revisions' ),
+        'taxonomies' => array( 'genre' ),
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'show_in_nav_menus' => true,
+        'publicly_queryable' => true,
+        'exclude_from_search' => false,
+        'has_archive' => true,
+        'query_var' => true,
+        'can_export' => true,
+        'rewrite' => true,
+        'capability_type' => 'post'
+    );
+    register_post_type( 'books', $args );
+}
+add_action( 'init', 'wppb_setup_post_types' );
